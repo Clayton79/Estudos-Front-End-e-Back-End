f11493480aa36b73dd4fa969db7b2443432e500a
Clayton79 <claytonxavier21s@gmail.com>
2025-01-13 22:09:13 -0300

    commit

---
+++ Node/Funcionarios/funcionario.js
@@ -0,0 +1,24 @@
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
+const axios = require("axios");
+
+axios.get(url).then(response => {
+const funcionarios = response.data;
+const menorSalarioChinesF = funcionarios.map(function(e) {
+    return e.salario;
+}).reduce(function(acumulador, atual) {
+    let oi = acumulador < atual ? acumulador : atual;
+    return oi;
+    // if(acumulador < atual) {
+    //     return acumulador
+    // } else {
+    //     return atual
+    // }
+    // console.log(acumulador, atual);
+    // return acumulador + atual 
+});
+    console.log(menorSalarioChinesF);
+});
+
+
+
+// fiz só peguei o salario menor de tudo só falta pegar a mulher da china
