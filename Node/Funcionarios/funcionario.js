e2224247a1538b9c186db471547bb7458a66f906
Clayton79 <claytonxavier21s@gmail.com>
2025-01-14 20:33:19 -0300

    commit

--- Node/Funcionarios/funcionario.js
+++ Node/Funcionarios/funcionario.js
@@ -1,24 +1,20 @@
 const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
 const axios = require("axios");
 
-axios.get(url).then(response => {
-const funcionarios = response.data;
-const menorSalarioChinesF = funcionarios.map(function(e) {
-    return e.salario;
-}).reduce(function(acumulador, atual) {
-    let oi = acumulador < atual ? acumulador : atual;
-    return oi;
-    // if(acumulador < atual) {
-    //     return acumulador
-    // } else {
-    //     return atual
-    // }
-    // console.log(acumulador, atual);
-    // return acumulador + atual 
+axios.get(url).then((response) => {
+  const funcionarios = response.data;
+
+  const menorSalarioChinesF = funcionarios
+    .map((funcionario) => {
+      if (funcionario.pais === "China" && funcionario.genero === "F") {
+        return funcionario.salario;
+      } else {
+        return Infinity;
+      }
+    })
+    .reduce((menor, salario) => {
+      return salario < menor ? salario : menor;
+    });
+
+  console.log(menorSalarioChinesF);
 });
-    console.log(menorSalarioChinesF);
-});
-
-
-
-// fiz só peguei o salario menor de tudo, só falta pegar a mulher e que seja chinesa ela.
